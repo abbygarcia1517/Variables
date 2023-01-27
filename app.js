4df28b556cb8995ea5a5cfe0a9449d55d3085422
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-01-26 22:54:08 -0600

    New Commit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var percentage = 100; //This is my battery percentage bar NUMBER
+let percentage = 100; // This is my battery percentage bar NUMBER
 
-var name = "Battery"; //This is the text used to display the battery percentage STRING
+percentage = percentage - 10;
+
+//90
+
+percentage = percentage - 10;
+
+//80
+
+percentage = percentage + 10;
+
+//90
+
+let text1 = "Battery"; // This is the text used to display the battery percentage STRING
 
 
-console.log("percentage");
+
+console.log(percentage);
+
+console.log(text1);
