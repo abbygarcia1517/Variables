dc6fa337766e38b5b9c068b247556f28a73f7aa9
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-01-26 20:44:39 -0600

    Commit 1

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// JavaScript source code
+var percentage = 100; //This is my battery percentage bar NUMBER
+
+var name = "Battery"; //This is the text used to display the battery percentage STRING
+
+
+console.log("Hi World")
