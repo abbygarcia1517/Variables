8f87378d731ef5536aaecc40ae240c55d334f46a
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-01-27 20:34:21 -0600

    Final

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+var datasignal = 100; // This is my phone data signal 
+// The code above is also my global variable 
+
 let percentage = 100; // This is my battery percentage bar NUMBER
+//This code above also is my block variable 
 
 percentage = percentage - 10;
 
@@ -14,8 +18,10 @@ percentage = percentage + 10;
 
 let text1 = "Battery"; // This is the text used to display the battery percentage STRING
 
-
+const dataprovider = {company:"PureTalk", dataplan:"unlimited", LTE:4};
 
 console.log(percentage);
 
 console.log(text1);
+
+console.log(datasignal);
